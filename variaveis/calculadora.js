c608c414ade84e8d088b25296b22a7cf8d195ca7
josineldo <josineldoo@gmail.com>
2023-03-14 13:59:35 -0300

    calculadora e lista de nomes

---
+++ variaveis/calculadora.js
@@ -0,0 +1,8 @@
+var x = 10;
+var y = 15;
+var soma = x + y
+var subtraçao = x - y 
+var multiplicaçao = x * y
+var divisao = x / y
+console.log(soma,subtraçao,multiplicaçao)
+
---
+++ variaveis/index.js
@@ -0,0 +1,32 @@
+var pessoa1 ={
+    nome: "lucas",
+    idade: 22,
+    cpf: "123.456.789-10"
+}
+var pessoa2={
+    nome: "joão",
+    idade: 22,
+    cpf: "124.324.789-09"
+}
+var pessoa3 ={
+    nome: "ana",
+    idade: 22,
+    cpf: "145.171.789-11"
+}
+console.log(pessoa1,pessoa2,pessoa3)
+
+
+
+
+
+
+
+
+
+
+var nome = "lucas"
+var idade = 24
+var cpf = "125.121.151-12"
+console.log(nome,idade,cpf)
+console.log(" ola meu sou :" + nome )
+
