db8a81e34c04fb47d3700b99cd79a09971eac017
josineldo <josineldoo@gmail.com>
2023-03-23 23:00:10 -0300

    desconto/cnh

---
+++ condicionais/desafiocond.js
@@ -0,0 +1,12 @@
+var idade = 18
+if(idade > 18){
+    console.log(`vc pode tirar a cnh`)
+ 
+} else if(idade == 18){
+    console.log(`tem desconto`)
+
+} else{
+    console.log(`vc não pode tirar cnh`)
+}
+
+
